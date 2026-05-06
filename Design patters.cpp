310330ccdc705b8ce76a76b33321d88c6f18930a
Roja E <roja.e@zensar.com>
2026-05-06 11:09:00 +0530

    added

--- Design patters.cpp
+++ Design patters.cpp
@@ -34,3 +34,79 @@ int main()
 	return 0;
 }
 //factory design pattern
+
+#include<iostream>
+using namespace std;
+class shape{
+    public:
+    virtual void draw()=0;
+    virtual ~shape(){}
+};
+class circle:public shape{
+    public:
+    void draw()override{
+        cout<<"drawing circle";}
+        
+};
+class rectangle:public shape{
+
+public:
+void draw()override{
+    cout<<"drawing rectangle";}
+};
+class shapefactory{
+public:
+static shape* createshape(const string type)
+{
+    if(type=="circle"){return new circle();}
+    if(type=="rectangle"){return new rectangle();}
+    else return nullptr;
+}
+};
+int main()
+{
+    shape *obj=shapefactory::createshape("rectangle");
+    obj->draw();
+    return 0;
+}
+
+//strategy design pattern
+#include<iostream>
+using namespace std;
+class paystrategy{
+ public:
+ virtual void pay()=0;
+ virtual ~paystrategy(){}
+};
+
+class gpay:public paystrategy{
+public:
+ void pay() override{
+    cout<<"pay through gpay";}
+    
+};
+class phonepay:public paystrategy{
+public:
+void pay()override{
+cout<<"pay through phonepay";}
+};
+class strategy{
+ paystrategy *p;
+ public:
+ void setstrategy(paystrategy *obj)
+ { 
+     p=obj;
+ }
+ void notify()
+ { 
+     p->pay();
+ }
+    
+};
+int main()
+{   
+    strategy *s=new strategy();
+    s->setstrategy(new gpay());
+    s->notify();
+    return 0;
+}
